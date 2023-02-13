d8db97ec465e18bbcf9b106f8129c284c70b1393
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2023-02-13 15:08:54 +0100

    Switch logo to use next/image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,22 @@
+import Image from 'next/image'
+
 export function Header() {
-  return (<div className="flex h-24 items-center justify-center bg-slate-100 px-4">
-  <nav className="flex w-full max-w-5xl items-center justify-between gap-4">
-    <a href="/" title="Go home">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className="h-16" src="/pokemon-logo.svg" alt="Pokemon" />
-    </a>
-    <a className="text-xl font-bold hover:underline" href="/pokedex">Pokédex</a>
-  </nav>
-</div>
-)
+  return (
+    <div className="flex h-24 items-center justify-center bg-slate-100 px-4">
+      <nav className="flex w-full max-w-5xl items-center justify-between gap-4">
+        <a href="/" title="Go home">
+          <Image
+            alt="Pokemon"
+            className="h-16"
+            height={64}
+            src="/pokemon-logo.svg"
+            width={175}
+          />
+        </a>
+        <a className="text-xl font-bold hover:underline" href="/pokedex">
+          Pokédex
+        </a>
+      </nav>
+    </div>
+  )
 }
