fcf928fb1b9bffa68b70e01a6d62fa24e7266b26
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2023-02-13 14:14:48 +0100

    Add favicon and edit readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
       <body className={inter.className}>
         <Header />
         <main className="mx-auto max-w-5xl p-6">{children}</main>
