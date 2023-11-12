d1351774efd9d62f31ca5be0ca34ef1eaa667af3
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2023-11-12 21:53:08 +0100

    Add nice pretty font :)

--- components/PokemonTile.tsx
+++ components/PokemonTile.tsx
@@ -1,6 +1,12 @@
 import { getPokemonImage, Pokemon } from '@/utils/pokemon'
 import Link from 'next/link'
 import Image from 'next/image'
+import { Borel } from 'next/font/google'
+
+const font = Borel({
+  subsets: ['latin'],
+  weight: '400',
+})
 
 type PokemonTileProps = {
   loading?: boolean
@@ -10,7 +16,11 @@ type PokemonTileProps = {
 export function PokemonTile({ loading = false, pokemon }: PokemonTileProps) {
   return (
     <div className="aspect-[5/6] overflow-hidden rounded-xl border-4 border-slate-200 bg-slate-100 text-center shadow-lg hover:border-slate-500">
-      <Link href={`/pokemon/${pokemon.name}`} title={pokemon.name}>
+      <Link
+        href={`/pokemon/${pokemon.name}`}
+        title={pokemon.name}
+        className="flex h-full flex-col justify-between"
+      >
         <Image
           alt={`${pokemon.name} official artwork`}
           className="aspect-square p-4"
@@ -18,7 +28,9 @@ export function PokemonTile({ loading = false, pokemon }: PokemonTileProps) {
           src={loading ? '/loading.svg' : getPokemonImage(pokemon)}
           width={475}
         />
-        <span className="block truncate bg-slate-200 py-5 text-xl font-bold capitalize">
+        <span
+          className={`${font.className} block truncate bg-slate-200 py-5 text-xl font-bold capitalize`}
+        >
           {pokemon.name.replaceAll('-', ' ')}
         </span>
       </Link>
