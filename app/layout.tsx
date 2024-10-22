669425177d4ba40157115062ae90858420ab74d7
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2024-10-22 11:31:13 +0200

    Upgrade eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import { Inter } from 'next/font/google'
 import { Header } from '@/components/Header'
-
 import './globals.css'
-
-export const runtime = 'edge'
 
 const font = Inter({
   subsets: ['latin'],
